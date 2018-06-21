ec641a675c6c69814333ee00fc2a8677b975291f
WooKyoung Noh <wookay.noh@gmail.com>
2018-06-21 17:35:09 +0900

    eval broadcasted

--- test/broadcast/style.jl
+++ test/broadcast/style.jl
@@ -21,7 +21,7 @@ end
 function Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{G{T,N}}}, ::Type{ElType}) where {T,N,ElType}
     @info :similar (T,N,ElType)
     A = bc.args[1]
-    G(similar(Array{ElType}, axes(bc)), A.sr)
+    G(Array{ElType}(undef, length.(axes(bc))), A.sr)
 end
 
 function Base.getindex(x::G{T,N}, inds::Vararg{Int,N}) where {T,N}
@@ -34,10 +34,6 @@ function Base.setindex!(x::G{T,N}, val, inds::Vararg{Int,N}) where {T,N}
     x.data[inds...] = val
 end
 
-function Broadcast.broadcasted(::typeof(+), ::G{T,N}, ::Int) where {T,N}
-    @info :broadcasted
-end
-
 
 using DataLogger
 output = DataLogger.read_stdout() do
@@ -46,10 +42,17 @@ g  = G{Int,1}([5,6], "")
 g2 = Float32.(g)
 @info :g2 g2 isa G{Float32,1}
 
-@info :g3 g2 .+ 1
+eval(quote
 
+    @info :g3 $g2 .+ 1
 
-@info :g5 g2 .+ 1
+    function Broadcast.broadcasted(::typeof(+), ::G{T,N}, ::Int) where {T,N}
+        @info :broadcasted
+    end
+
+    @info :g5 $g2 .+ 1
+
+end)
 
 end
 
@@ -64,9 +67,52 @@ Record ( Main style.jl#28 ) getindex
 Record ( Main style.jl#33 ) setindex
 Record ( Main style.jl#28 ) getindex
 Record ( Main style.jl#33 ) setindex
-Record ( Main style.jl#47 ) g2   g2 isa G{Float32, 1} = true
-Record ( Main style.jl#38 ) broadcasted
-Record ( Main style.jl#49 ) g3   g2 .+ 1 = nothing
-Record ( Main style.jl#38 ) broadcasted
-Record ( Main style.jl#52 ) g5   g2 .+ 1 = nothing
+Record ( Main style.jl#43 ) g2   g2 isa G{Float32, 1} = true
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#12 ) BroadcastStyle
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#22 ) similar   (T, N, ElType) = (Float32, 1, Float32)
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#33 ) setindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#33 ) setindex
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#17 ) size
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#28 ) getindex
+Record ( Main style.jl#47 ) g3   Float32[5.0, 6.0] .+ 1 = Float32[6.0, 7.0]
+Record ( Main style.jl#50 ) broadcasted
+Record ( Main style.jl#53 ) g5   Float32[5.0, 6.0] .+ 1 = nothing
 """
