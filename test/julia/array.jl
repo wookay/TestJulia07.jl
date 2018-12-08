74b24cee1dd60d6c160fff12cbfaf482b59ac4a9
WooKyoung Noh <wookay.noh@gmail.com>
2018-12-09 06:21:19 +0900

    Jive runtests

--- test/julia/array.jl
+++ test/julia/array.jl
@@ -20,4 +20,16 @@ A = [6, 5, 4, 3, 2, 1]
 
 @test circshift(1:3, 1) == [3, 1, 2]
 
-end
+end # module test_julia_array
+
+
+using Jive
+@If VERSION >= v"1.1.0-DEV.792" module test_julia_array_eachrow
+
+using Test
+
+A = [1 2; 3 4; 5 6]
+@test collect(eachrow(A)) == [[1, 2], [3, 4], [5, 6]]
+@test collect(eachcol(A)) == [[1, 3, 5], [2, 4, 6]]
+
+end # @If VERSION >= v"1.1.0-DEV.792" module test_julia_array_eachrow
---
+++ test/julia/ccall.jl
@@ -0,0 +1,7 @@
+module test_julia_ccall
+
+using Test
+
+# https://github.com/JuliaLang/julia/blob/master/base/essentials.jl#L339
+
+end # module test_julia_ccall
