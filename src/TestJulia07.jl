9fa45d5b79ac85bce3c82b15dce441ebfcea946a
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-11 22:31:52 +0900

    Pkg

--- src/TestJulia07.jl
+++ src/TestJulia07.jl
@@ -1,10 +1,12 @@
 module TestJulia07
 
+using Pkg
+
 # julia --color=yes -e 'using TestJulia07; TestJulia07.pull(split("Metalhead Flux"))'
 function pull(pkgs::Vector)
     for pkg in pkgs
         printstyled(pkg, ' ', color=:yellow)
-        path = normpath(ENV["HOME"], ".julia", "dev", pkg)
+        path = normpath(Pkg.devdir(), pkg)
         cd(path)
         run(`git pull`)
     end
---
+++ test/testjulia07/pull.jl
@@ -0,0 +1,8 @@
+module test_testjulia07_pull
+
+using Test
+using TestJulia07
+
+@test TestJulia07.pull isa Function 
+
+end
