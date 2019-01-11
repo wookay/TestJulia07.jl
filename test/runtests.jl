acc3f66bb6935b7488b666f8ae7f04d8129290cd
WooKyoung Noh <wookay.noh@gmail.com>
2019-01-11 18:31:27 +0900

    node1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,3 @@
 using Jive # runtests Jive.Skipped
-runtests(@__DIR__)
+runtests(@__DIR__, node1=["stdlib/sharedarrays.jl"])
 !isempty(Jive.Skipped.modules) && printstyled("skipped tests: ", join(Jive.Skipped.modules, ", "), "\n", color=:yellow)
