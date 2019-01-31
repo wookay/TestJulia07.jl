b025d323b486665edd8172fa684d47cb60932817
WooKyoung Noh <wookay.noh@gmail.com>
2019-02-01 01:25:42 +0900

    update musicmanipulations

--- test/pkgs/musicmanipulations/notes.jl
+++ test/pkgs/musicmanipulations/notes.jl
@@ -1,11 +1,9 @@
 module test_pkgs_musicmanipulations
 
-using MusicManipulations
 using Test
+using MusicManipulations
 
-@test pitch_to_name(55) == "G4"
-@test pitch_to_name(0) == "C0"
-@test pitch_to_name(1) == "C♯0"
-@test pitch_to_name(2) == "D0"
+c4 = Note(60, 0, 0, 1)
+@test pitch_to_name(c4.pitch) == "C4"
 
 end # module test_pkgs_musicmanipulations
