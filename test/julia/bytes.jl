38d829c0c4aa530083704abb094904d6f5c37635
WooKyoung Noh <wookay.noh@gmail.com>
2019-12-12 22:03:19 +0900

    b_str

--- test/julia/bytes.jl
+++ test/julia/bytes.jl
@@ -9,3 +9,12 @@ u32 = Vector{UInt32}([0x04030201])
 @test reinterpret(UInt8, u32) == u8
 
 end # module test_julia_bytes
+
+
+module test_julia_bytes_b_str
+
+using Test
+
+@test b"hello" == @b_str("hello") == [0x68, 0x65, 0x6c, 0x6c, 0x6f] 
+
+end # module test_julia_bytes_b_str
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,5 @@
 using Jive
-runtests(@__DIR__, skip=["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead"], node1=["stdlib/sharedarrays", "stdlib/distributed"])
+skips = ["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead"]
+haskey(ENV, "TRAVIS") && push!(skips, "julia/inbounds")
+runtests(@__DIR__, skip=skips, node1=["stdlib/sharedarrays", "stdlib/distributed"])
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
