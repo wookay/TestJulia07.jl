7384721742f026554009e635cf9c1985d300bb31
WooKyoung Noh <wookay.noh@gmail.com>
2020-06-04 14:01:43 +0900

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 using Jive
-skips = ["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead"]
+skips = ["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead", "pkgs/cassette", "pkgs/temporal"]
 if haskey(ENV, "TRAVIS")
     push!(skips, "julia/inbounds")
-    ENV["TRAVIS_JULIA_VERSION"] == "nightly" && append!(skips, ["pkgs/flux", "pkgs/cassette"])
+    ENV["TRAVIS_JULIA_VERSION"] == "nightly" && append!(skips, ["pkgs/flux"])
 end
 runtests(@__DIR__, skip=skips, node1=["stdlib/sharedarrays", "stdlib/distributed"])
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
