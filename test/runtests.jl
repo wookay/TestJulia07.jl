4c51bb1119b9087af18319b5ae3e9aae8392a371
WooKyoung Noh <wookay.noh@gmail.com>
2020-06-04 12:26:19 +0900

    skip pkgs/flux, pkgs/cassette

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Jive
 skips = ["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead"]
-haskey(ENV, "TRAVIS") && push!(skips, "julia/inbounds")
+if haskey(ENV, "TRAVIS")
+    push!(skips, "julia/inbounds")
+    ENV["TRAVIS_JULIA_VERSION"] == "nightly" && append!(skips, ["pkgs/flux", "pkgs/cassette"])
+end
 runtests(@__DIR__, skip=skips, node1=["stdlib/sharedarrays", "stdlib/distributed"])
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
