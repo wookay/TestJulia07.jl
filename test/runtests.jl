a01f4cf774ea0cb907906c798edeb43d0c7da9ce
WooKyoung Noh <wookay.noh@gmail.com>
2020-07-02 12:45:48 +0900

    skip test flux

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using Jive
 skips = ["revise.jl", "pkgs/revise", "pkgs/cstparser", "pkgs/flows", "pkgs/metalhead", "pkgs/cassette", "pkgs/temporal"]
 if haskey(ENV, "TRAVIS")
     push!(skips, "julia/inbounds")
-    ENV["TRAVIS_JULIA_VERSION"] == "nightly" && append!(skips, ["pkgs/flux"])
+    if VERSION >= v"1.3"
+        append!(skips, ["pkgs/flux"])
+    end
 end
 runtests(@__DIR__, skip=skips, node1=["stdlib/sharedarrays", "stdlib/distributed"])
 !isempty(Jive.Skipped.expressions) && printstyled("skipped tests: ", join(Jive.Skipped.expressions, ", "), "\n", color=:yellow)
