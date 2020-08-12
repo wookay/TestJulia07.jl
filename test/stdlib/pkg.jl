0350a3dbab2863f913a9f4bd13662f2702b94e3c
WooKyoung Noh <wookay.noh@gmail.com>
2020-08-13 05:32:57 +0900

    pkg

--- test/stdlib/pkg.jl
+++ test/stdlib/pkg.jl
@@ -1,9 +1,9 @@
 module test_stdlib_pkg
 
 using Test
-using Pkg
 using UUIDs
 using Jive
+using Pkg
 
 pkg = Base.identify_package(Pkg, "Pkg")
 @test pkg.uuid == UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f")
