260ccfe9fa115027201d413e055904b466ffe310
WooKyoung Noh <wookay.noh@gmail.com>
2020-08-13 13:56:21 +0900

    pkg

--- test/stdlib/pkg.jl
+++ test/stdlib/pkg.jl
@@ -1,19 +1,14 @@
 module test_stdlib_pkg
 
 using Test
-using UUIDs
-using Jive
 using Pkg
 
 pkg = Base.identify_package(Pkg, "Pkg")
-@test pkg.uuid == UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f")
+@test pkg.uuid == Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f")
 
 if VERSION >= v"1.4"
     deps = Pkg.dependencies()
-    for dep in deps
-        @info :dep dep
-    end
-    uuid = Base.UUID("ba5e3d4b-8524-549f-bc71-e76ad9e9deed") # Jive
+    uuid = Base.UUID("76f85450-5226-5b5a-8eaa-529ad045b433") # LibGit2
     @test deps[uuid].version isa VersionNumber
 end
 
