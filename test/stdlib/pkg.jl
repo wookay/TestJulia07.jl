403123158cd102dec4e0353ff0b8bcf05a3c8641
WooKyoung Noh <wookay.noh@gmail.com>
2020-08-13 18:31:09 +0900

    pkg

--- test/stdlib/pkg.jl
+++ test/stdlib/pkg.jl
@@ -2,14 +2,13 @@ module test_stdlib_pkg
 
 using Test
 using Pkg
-using LibGit2
 
 pkg = Base.identify_package(Pkg, "Pkg")
 @test pkg.uuid == Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f")
 
 if VERSION >= v"1.4"
     deps = Pkg.dependencies()
-    uuid = Base.UUID("76f85450-5226-5b5a-8eaa-529ad045b433") # LibGit2
+    uuid = Base.UUID("8dfed614-e22c-5e08-85e1-65c5234f0b40") # Test
     @test deps[uuid].version isa VersionNumber
 end
 
