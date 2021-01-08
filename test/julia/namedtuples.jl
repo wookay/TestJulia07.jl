0a0a0e3cfc865814de61c7fabcb1991250ea90ce
WooKyoung Noh <wookay.noh@gmail.com>
2021-01-08 13:58:45 +0900

    more namedtuple

--- test/julia/namedtuples.jl
+++ test/julia/namedtuples.jl
@@ -3,7 +3,7 @@ module test_julia_namedtuples
 using Test
 
 @test (a=1, b=2) == (a=1, :b=>2)
-@test (; :b=>2) == (b=2,)
+@test (b=2,) == (; b=2) == (; :b=>2)
 @test NamedTuple{(:a, :b),Tuple{Int,Int}}((1, 2)) == NamedTuple{(:a, :b)}((1, 2)) == (a=1, b=2)
 
 d = Dict(:a=>1, :b=>2)
