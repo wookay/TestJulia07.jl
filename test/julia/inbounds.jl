1207b91c0e7d3453fdcc305ac083af2e5bd75871
WooKyoung Noh <wookay.noh@gmail.com>
2021-03-12 16:12:36 +0900

    fix inbounds

--- test/julia/inbounds.jl
+++ test/julia/inbounds.jl
@@ -2,11 +2,8 @@ module test_julia_inbounds
 
 using Test
 
-@test_throws BoundsError for i in [0, 1, 2]
-                             getindex([10], i)
-                         end
-
 if Base.JLOptions().check_bounds != 1  # --check-bounds=yes
+
 vals = []
 @inbounds for i in [0, 1, 2]
               c = getindex([10], i)
@@ -20,6 +17,13 @@ for i in [0, 1, 2]
     push!(vals, c)
 end
 @test ==(10).(vals) == [false, true, false]
+
+else # if Base.JLOptions().check_bounds != 1
+
+@test_throws BoundsError for i in [0, 1, 2]
+                             getindex([10], i)
+                         end
+
 end # if Base.JLOptions().check_bounds != 1
 
 end # module test_julia_inbounds
