e62996fd3a05f6a52733eb04323ede8887494c15
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-14 12:02:28 +0900

    repeat, circshift

--- test/julia/abstractarraymath.jl
+++ test/julia/abstractarraymath.jl
@@ -1,4 +1,4 @@
-module test_julia_abstractarraymath
+module test_julia_abstractarraymath_eachrow_eachcol_eachslice
 
 using Test
 
@@ -15,4 +15,24 @@ using Test
 @test collect(eachslice([1 2 3;], dims=1)) == [[1, 2, 3]]
 @test collect(eachslice([1 2 3;], dims=2)) == [[1], [2], [3]]
 
-end # module test_julia_abstractarraymath
+end # module test_julia_abstractarraymath_eachrow_eachcol_eachslice
+
+
+module test_julia_abstractarraymath_repeat_circshift
+
+using Test
+
+@test repeat([1; 2; 3], 1, 5) == [1 1 1 1 1;
+                                  2 2 2 2 2;
+                                  3 3 3 3 3;]
+
+@test repeat([1 2 3;], 5, 1) == [1 2 3;
+                                 1 2 3;
+                                 1 2 3;
+                                 1 2 3;
+                                 1 2 3;]
+
+@test circshift([1, 2, 3, 4, 5], 3) ==
+                      [3, 4, 5, 1, 2]
+
+end # module test_julia_abstractarraymath_repeat_circshift
