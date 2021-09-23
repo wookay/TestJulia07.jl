af90cba427f05971d67276512b64d716fa04d90e
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-23 16:31:27 +0900

    missing, rand

--- test/julia/missing.jl
+++ test/julia/missing.jl
@@ -2,11 +2,27 @@ module test_julia_missing
 
 using Test
 
+@test missing === missing
+@test ismissing(missing)
+@test skipmissing([missing]) isa Base.SkipMissing{Vector{Missing}}
+@test collect(skipmissing([])) == Any[]
+@test collect(skipmissing(Union{Missing}[missing])) == Union{}[]
+@test collect(skipmissing(Union{Int,Missing}[missing])) == Int[]
+
 @test (missing in []    ) === false
-@test (missing in [1]   ) === missing
 @test (missing in ()    ) === false
-@test (missing in (1,)  ) === missing
 @test (missing in Set() ) === false
 @test (missing in Set(1)) === false
+
+@test ismissing(missing == missing)
+@test ismissing(missing in [missing])
+@test ismissing(missing in [1]   )
+@test ismissing(missing in (1,)  )
+
+@test missing in keys(Dict(missing => 1))
+@test ismissing(missing in values(Dict(1 => missing)))
+
+@test ismissing(missing in collect(keys(Dict(missing => 1))))
+@test ismissing(missing in collect(values(Dict(1 => missing))))
 
 end # module test_julia_missing
--- test/stdlib/random.jl
+++ test/stdlib/random.jl
@@ -1,8 +1,23 @@
 module test_stdlib_random
 
 using Test
-using Random: Sampler, MersenneTwister
+using Random: Random, Sampler, MersenneTwister
 
-@test rand(Sampler(MersenneTwister, 1:10)) <= 10
+const rand_nums = let
+    if VERSION >= v"1.8.0-DEV.575"
+        [5, 1]
+    else
+        [5, 8]
+    end
+end
+
+
+Random.seed!(0)
+@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[1]
+@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[2]
+
+Random.seed!(0)
+@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[1]
+@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[2]
 
 end # module test_stdlib_random
