710f5dc8c4db1cb3e61675f37ce35ad9258165bd
WooKyoung Noh <wookay.noh@gmail.com>
2022-05-10 10:55:22 +0900

    Splat

--- test/julia/splat.jl
+++ test/julia/splat.jl
@@ -2,6 +2,10 @@ module test_julia_splat
 
 using Test
 
+if VERSION >= v"1.9.0-DEV.513"
+@test Base.Splat(+)((1,2,3)) == .+(1,2,3) == 6
+else
 @test Base.splat(+)((1,2,3)) == .+(1,2,3) == 6
+end
 
 end # module test_julia_splat
