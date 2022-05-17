e6bb0ddbe7035ee05cf3ff350c8c5a7cc4014bd5
WooKyoung Noh <wookay.noh@gmail.com>
2022-05-18 01:00:40 +0900

    move test_julia_destructure_callex to TestJulia11

--- test/julia/reflection.jl
+++ test/julia/reflection.jl
@@ -26,26 +26,3 @@ g(; b, c, a) = a + b + c
 @test hasmethod(g, Tuple{}, (:a, :b, :c))
 
 end # @If v"1.2.0-DEV.218" >= VERSION module test_reflection_hasmethod
-
-
-module test_julia_destructure_callex
-
-using Test
-
-macro destructure(ex)
-    f, args, kwargs = Base.destructure_callex(ex)
-    (f, args, kwargs)
-end
-
-(f, args, kwargs) = @destructure f(a::Int; b=2)
-@test f === :f
-@test args == Any[:(a::Int)]
-@test kwargs == Any[Expr(:kw, :b, 2)]
-
-@test (f, args, kwargs) == Base.destructure_callex(:(f(a::Int; b=2)))
-
-q = quote f(a::Int; b=2) end
-@test q.args[1] isa LineNumberNode
-@test :(f(a::Int; b=2)) == q.args[2]
-
-end # module test_julia_destructure_callex
