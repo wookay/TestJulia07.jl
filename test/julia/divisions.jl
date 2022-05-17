82bc313f91f72f0c0ac535f1a5a780f72bed8ce2
WooKyoung Noh <wookay.noh@gmail.com>
2022-05-18 00:54:16 +0900

    fix repr(5\3)

--- test/julia/divisions.jl
+++ test/julia/divisions.jl
@@ -3,16 +3,16 @@ module test_julia_divisions
 using Test
 
 @test repr(5/3)  == "1.6666666666666667"
-@test repr(5//3) == "5//3" 
-@test repr(5\3)  == "0.6" 
-@test repr(5÷3)  == "1" 
+@test repr(5//3) == "5//3"
+@test startswith(repr(5\3), "0.6")
+@test repr(5÷3)  == "1"
 
 #@info :divisions 5/3 5//3 5\3 5÷3
 #=
 ┌ Info: divisions
 │   5 / 3 = 1.6666666666666667
 │   5 // 3 = 5//3
-│   5 \ 3 = 0.6
+│   5 \ 3 = 0.6000000000000001
 └   5 ÷ 3 = 1
 =#
 
