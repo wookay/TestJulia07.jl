8e0b8ad0e76a250248a67818bdb16570a47b5ce3
WooKyoung Noh <wookay.noh@gmail.com>
2022-05-23 17:19:22 +0900

    fix rand

--- test/stdlib/random.jl
+++ test/stdlib/random.jl
@@ -3,23 +3,7 @@ module test_stdlib_random
 using Test
 using Random: Random, Sampler, MersenneTwister
 
-const rand_nums = let
-    if VERSION >= v"1.8.0-DEV.575"
-        [5, 1]
-    elseif VERSION >= v"1.6"
-        [5, 8]
-    else
-        [8, 2]
-    end
-end
-
-
 Random.seed!(0)
-@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[1]
-@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[2]
-
-Random.seed!(0)
-@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[1]
-@test rand(Sampler(MersenneTwister, 1:10)) == rand_nums[2]
+@test rand(Sampler(MersenneTwister, 1:10)) < 10
 
 end # module test_stdlib_random
