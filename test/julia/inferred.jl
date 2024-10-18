e1795d6c141990fab3ceacc600668cb7fd98ef8a
WooKyoung Noh <wookay.noh@gmail.com>
2024-10-19 06:53:29 +0900

    test inferred

--- test/julia/inferred.jl
+++ test/julia/inferred.jl
@@ -7,15 +7,24 @@ function f1(a::Bool)
 end
 
 function f2(a::Bool)::Vector{Any}
-       [a ? 1 : 1.0]
+    [a ? 1 : 1.0]
 end
 
 function f3(a::Bool)
-       [a ? 1 : 1.0]
+    [a ? 1 : 1.0]
+end
+
+function f4(a::Bool)
+    a ? 1 : "hello"
 end
 
                        @inferred(f1(true))
                        @inferred(f2(true))
+
+# ERROR: LoadError: return type Vector{Int64} does not match inferred return type Union{Vector{Float64}, Vector{Int64}}
 @test_throws Exception @inferred(f3(true))
+
+# ERROR: LoadError: return type Int64 does not match inferred return type Union{Int64, String}
+@test_throws Exception @inferred(f4(true))
 
 end # module test_julia_inferred
