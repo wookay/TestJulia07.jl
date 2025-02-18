dae86e1669991134b67dfeba98f3ea8093f9e741
WooKyoung Noh <wookay.noh@gmail.com>
2025-02-19 08:36:10 +0900

    test struct comparison, package_callbacks

--- test/julia/comparison.jl
+++ test/julia/comparison.jl
@@ -15,18 +15,29 @@ struct A
     bs::Vector{B}
 end
 
-for typ in (A, B)
-    @eval function Base.:(==)(l::T, r::T) where {T <: $typ}
-        for name in fieldnames(T)
-            getfield(l, name) != getfield(r, name) && return false
-        end
-        return true
-    end
+# for typ in (A, B)
+#     @eval function Base.:(==)(l::T, r::T) where {T <: $typ}
+#         for name in fieldnames(T)
+#             getfield(l, name) != getfield(r, name) && return false
+#         end
+#         return true
+#     end
+# end
+
+function Base.:(==)(l::T, r::T) where {T <: B}
+    return l.cs == r.cs
 end
+
+function Base.:(==)(l::T, r::T) where {T <: A}
+    return l.bs == r.bs
+end
+
 
 x = [A([B([C(1, 2)])])]
 y = [A([B([C(1, 2)])])]
+z = [A([B([C(5, 6)])])]
 
 @test x == y
+@test y != z
 
 end # module test_julia_comparison
