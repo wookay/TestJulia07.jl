5caaa4fc5678237915d892b2d8b2f6b1e2dc7369
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-26 09:00:56 +0900

    test Base.showarg

--- test/julia/show.jl
+++ test/julia/show.jl
@@ -7,4 +7,7 @@ ioc = IOContext(buf, :SHOWN_SET => 5)
 Base.show_circular(ioc, 5)
 @test String(take!(buf)) == "#= circular reference @-1 =#"
 
+@test sprint(Base.showarg, 1, true) == "Int64"
+@test sprint(Base.showarg, 1, false) == "::Int64"
+
 end # module test_julia_show
