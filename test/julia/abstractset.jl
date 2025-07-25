9f778b27cfa560ccae8a2c03b8211f80e038af02
WooKyoung Noh <wookay.noh@gmail.com>
2025-07-25 22:23:17 +0900

    test IdSet

--- test/julia/abstractset.jl
+++ test/julia/abstractset.jl
@@ -4,4 +4,12 @@ using Test
 
 @test union((1,3,5,5,5)) == union([1,3,5,5,5]) == [1,3,5]
 
+s1 = Set{Any}(Any[true, 1, 1.0])
+@test length(s1) == 1
+@test collect(s1) == Any[1.0]
+
+s2 = IdSet{Any}(Any[true, 1, 1.0])
+@test length(s2) == 3
+@test collect(s2) == Any[true, 1, 1.0]
+
 end # module test_julia_abstractset
